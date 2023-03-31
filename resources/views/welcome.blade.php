9b989b0f1324562b629942145880237406be8c73
bagusadisaka <bagusadisaka06@gmail.com>
2023-03-31 09:32:15 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+  <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+  <!-- @vite('resources/css/app.css') -->
+</head>
+<body>
+
+
+    <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between ml-[9vw] p-4">
+        <a href="#" class="flex items-center">
+            <!-- <img src="assets/Logo Liga.png" class="h-8 mr-3" alt="Logo" /> -->
+            <span class="self-center text-2xl font-font-1 whitespace-nowrap dark:text-white">Portofolio</span>
+        </a>
+        <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+        <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Profile</a>
+            </li>
+            <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Skills</a>
+            </li>
+            <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Interest</a>
+            </li>
+            <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Educations</a>
+            </li>
+            <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Organizations</a>
+            </li>
+            <li>
+            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Achievements</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+
+
+    <h1></h1>
+
+    <div id="profile" class=" border-green flex flex-row">
+        <!-- <p class="text-[20vw] h-[10vw] w-full">Hello!</p> -->
+        <div class="w-5/6">
+            <h1 class="text-[3vw] font-bold font-font-2 mt-[4vw] ml-[9vw]">
+                Hello!
+            </h1>
+            <h2 class="text-[1.5vw] font-semi bold ml-[9vw] mt-[0.5vw]">
+                I'm Nandatama Bagus Adisaka
+            </h2>
+            <p class="text-[1vw] mt-[2vw] mr-[10vw] ml-[9vw] text-justify ">
+                Someone who interested and pursuing technology and information. Oriented to future technology development. Communication and teamwork are some of the keys to success. Become a leader and successor of technology development
+            </p>
+        </div>
+        <div class="justify-center flex w-[50vw]">
+            <img src="assets/Foto Profile.png" class="h-[25vw] mt-[1vw] absolute z-10"/>
+            <img src="assets/Background Profile.png" class="h-[25vw] mt-[1vw] relative z-0"/>
+        </div>   
+    </div>
+
+    <div>
+        <img src="assets/Line.png" class="h-[0.07vw] ml-[9vw]"/>
+    </div>
+
+    <div id="skills" class=" border-green flex flex-row">
+        <div class="justify-center flex w-[100vw]">
+            <img src="assets/Skills.png" class="h-[25vw] mt-[1vw] absolute z-10"/>
+            <img src="assets/Design Skills 1.png" class="h-[10vw] ml-[5vw] relative z-0"/>
+        </div>   
+    </div>
+
+
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {    
+      fontFamily: {
+      'font-1': ['"Cedarville Cursive"', 'cursive'],
+      'font-2': ['"Kaushan Script"', 'cursive'],   
+      },
+    },
+  },
+  plugins: [],
+}
+
